e375b1f64945ce74b50fd1fcfda6531747ec515e
Radek <radek.palisa@gmail.com>
2023-08-13 10:18:49 +0200

    custom keymap

---
+++ keyboards/splitkb/kyria/keymaps/Radek-Palisa/rules.mk
@@ -0,0 +1,11 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+# Convert from Elite-C to Liatris
+CONVERT_TO=liatris
+
+# qmk c2json -km Radek-Palisa -kb splitkb/kyria/rev3 /Users/radekpalisa/qmk_firmware/keyboards/splitkb/kyria/keymaps/Radek-Palisa/keymap.c
+
